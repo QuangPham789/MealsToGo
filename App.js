00b3d8261e19cf5d23abfeedfbc70c5794807e0d
QuangPham <nhutquang23@gmail.com>
2021-08-10 21:02:51 +0700

    commit searchBar

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusbar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, Platform, StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar, SafeAreaView, Platform, StyleSheet, Text, View } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
     <>
-      <SafeAreaView style={{flex:1}}>
-        <View style={{padding:16, backgroundColor:'green'}}>
-          <Text>Search</Text>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight() }}>
+        <View style={{ padding: 16, backgroundColor: 'green' }}>
+          <Searchbar placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery} />
         </View>
 
-        <View style={{flex: 1, backgroundColor:'blue'}}>
+        <View style={{ padding: 16, flex: 1, backgroundColor: 'blue' }}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
+      <ExpoStatusbar style='auto' />
     </>
   );
 }
